302082efd3723cf8fc42dc2ea26204a7aa4e35d9
arfrie22 <43021241+arfrie22@users.noreply.github.com>
2024-05-06 11:12:09 -0400

    added copy button

--- src/lib/Viewer.svelte
+++ src/lib/Viewer.svelte
@@ -2,14 +2,17 @@
 	import { Draw as DrawCourse } from './viewer/Draw';
 	import type { CourseData } from './convert';
 	import { Paginator } from '@skeletonlabs/skeleton';
+	import { getToastStore } from '@skeletonlabs/skeleton';
+
+	const toastStore = getToastStore();
 
 	let scale = 16;
-	
-	export let id = "";
+
+	export let id = '';
 	export let courses: CourseData[];
 	let paginatorSettings = { amounts: [], size: courses.length, page: 1, limit: 1 };
 
-	let divId = "level-viewer-container-"+id;
+	let divId = 'level-viewer-container-' + id;
 
 	async function setUp(courses: CourseData[], scale: number) {
 		const courseData = courses[paginatorSettings.page];
@@ -29,6 +32,50 @@
 		divId = elment.id;
 		setUp(courses, scale);
 	}
+
+	function copyImageToClipboard() {
+		const canvas = document.getElementById(`level-viewer-container-${id}_courseDraw`) as HTMLCanvasElement;
+		if (!canvas) {
+			toastStore.trigger({
+				message: 'No level to copy',
+				background: 'variant-filled-error',
+			});
+			return;
+		}
+		const img = canvas.toBlob((blob) => {
+			try {
+				if (!navigator.clipboard) {
+					throw new Error('Clipboard API not available');
+				}
+
+				if (!blob) {
+					throw new Error('No image data');
+				}
+				navigator.clipboard.write([
+					new ClipboardItem({
+						'image/png': blob
+					})
+				]);
+
+				toastStore.trigger({
+					message: 'Image copied to clipboard',
+					background: 'variant-filled-success',
+				});
+			} catch (e) {
+				let errorMessage: string;
+				if (e instanceof Error) {
+					errorMessage = e.message;
+				} else {
+					errorMessage = 'An unknown error occurred';
+				}
+
+				toastStore.trigger({
+					message: errorMessage,
+					background: 'variant-filled-error',
+				});
+			}
+		});
+	}
 </script>
 
 <div class="level-height flex flex-1 flex-col gap-2 overflow-hidden">
@@ -40,19 +87,26 @@
 			<option value={32}>Large</option>
 		</select>
 	</label>
-	<div id={"level-viewer-container-"+id} class="flex-1 overflow-scroll" use:init>
+	<div id={'level-viewer-container-' + id} class="flex-1 overflow-scroll" use:init>
 		<canvas id="canvas"></canvas>
 	</div>
-	{#if courses.length > 1}
-	<Paginator
-			showFirstLastButtons={true}
-			showPreviousNextButtons={true}
-			showNumerals
-			maxNumerals={1}
-			settings={paginatorSettings}
-			on:page={() => {
-				setUp(courses, scale);
-			}}
-	/>
-	{/if}
+	<div class="flex justify-between gap-2 w-full px-4">
+		
+		{#if courses.length > 1}
+			<Paginator
+				showFirstLastButtons={true}
+				showPreviousNextButtons={true}
+				showNumerals
+				maxNumerals={1}
+				settings={paginatorSettings}
+				on:page={() => {
+					setUp(courses, scale);
+				}}
+			/>
+		{/if}
+		<button type="button" class="btn variant-filled" on:click={copyImageToClipboard}>
+			<span class="icon"><i class="bx bxs-copy-alt" /></span>
+			<span>Copy Level as Image</span>
+		</button>
+	</div>
 </div>
